827e4efd27e618b2e4d16dce37cca2508031f11a
Haimis <haimila.mikko@gmail.com>
2020-03-18 23:44:36 +0200

    main pois testikattavuusraportista

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package scrabblebotti;
+package main;
+
+import scrabblebotti.Dictionary;
 
 /**
  *
