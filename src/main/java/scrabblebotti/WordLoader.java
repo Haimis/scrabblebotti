f5327bfaa8b60730777c1a0235b979942bf7a6fc
Haimis <haimila.mikko@gmail.com>
2020-03-20 05:26:36 +0200

    javadoc käyttöön ja kommentit WordLoader luokalle

--- src/main/java/scrabblebotti/WordLoader.java
+++ src/main/java/scrabblebotti/WordLoader.java
@@ -7,16 +7,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class <code>WordLoader</code> allows user to read file with word list.
+ * 
+ * 
+ * 
+ */
 public class WordLoader {
     BufferedReader br;
     
+    /**
+     * Creates a new BufferedReader with a new FileReader with.
+     *  automatically designated source file.
+     * @throws FileNotFoundException 
+     */
     public void openFile() throws FileNotFoundException {
-        br = new BufferedReader(new FileReader("sanalista.txt"));
-
-        
-        
+        br = new BufferedReader(new FileReader("sanalista.txt"));  
     }
     
+    /**
+     * Reads the designated source file, adds every word from it to a list object.
+     * @return return List with words written from source file.
+     * @throws IOException 
+     */
     public List<String> readFile() throws IOException {
         List<String> list = new ArrayList<>();
         String line;
@@ -28,6 +41,10 @@ public class WordLoader {
 
     }
     
+    /**
+     * Closes the BufferedReader.
+     * @throws IOException 
+     */
     public void closeFile() throws IOException {
         br.close();
     }
